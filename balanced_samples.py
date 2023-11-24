f49242b207039acac98896ccf5bac62c1c8bb4d8
JenniferTsang911 <jennifer.t-911@hotmail.com>
2023-11-24 18:07:30 -0500

    updated balanced_samples

--- balanced_samples.py
+++ balanced_samples.py
@@ -1,36 +1,43 @@
 import pandas as pd
-import random
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import torch
 
 # Load the CSV dataset into a Pandas DataFrame
-df = pd.read_csv('your_file_path.csv', header=None)
+df = pd.read_csv(r"C:\Users\tsang\OneDrive - Queen's University\DESI project\DESI TXT colon\Annotated Dataset\2021 03 30 colon 0720931-3 Analyte 5_dataset.csv", header=0)
 
 # Ignore the first column of the DataFrame
 df = df.iloc[:, 1:]
 
 # Find unique classes in the second column
-unique_classes = df[1].unique()
+unique_classes = df.iloc[:, 0].unique()
 
 # Create a dictionary to store the data frames for each class
-class_dataframes = {}
-
-# Split the data into separate DataFrames based on the unique classes
-for class_name in unique_classes:
-    class_dataframes[class_name] = df[df[1] == class_name]
+class_dataframes = {class_name: df[df.iloc[:, 0] == class_name] for class_name in unique_classes}
 
 # Find the minimum number of samples among the classes
-min_samples = min(len(class_dataframes[class_name]) for class_name in unique_classes)
+min_samples = min(len(class_df) for class_df in class_dataframes.values())
 
 # Randomly select and remove rows from classes with more samples
-for class_name in unique_classes:
-    if len(class_dataframes[class_name]) > min_samples:
-        # Randomly shuffle the rows in the DataFrame
-        class_dataframes[class_name] = class_dataframes[class_name].sample(frac=1, random_state=1)
-
-        # Keep only the first 'min_samples' rows
-        class_dataframes[class_name] = class_dataframes[class_name].head(min_samples)
+for class_name, class_df in class_dataframes.items():
+    if len(class_df) > min_samples:
+        class_dataframes[class_name] = class_df.sample(min_samples)
 
 # Concatenate the dataframes for each class back into a single dataframe
 balanced_df = pd.concat(class_dataframes.values())
 
 # Save the balanced dataset to a new CSV file
-balanced_df.to_csv('balanced_dataset.csv', header=False, index=False)
+balanced_df.to_csv(r"2021 03 30 colon 0720931-3 Analyte 5_dataset_balanced.csv", index=False)
+
+# # Load the dataset
+# df = pd.read_csv(r"C:\Users\tsang\Documents\GitHub\dc-DeepMSI\balanced_dataset.csv", header=0)
+
+# # Assuming the column name for labels is 'tissue_type'
+# labels = df['Class'].values
+
+# # Convert labels to numerical values if they are categorical
+# le = LabelEncoder()
+# labels = le.fit_transform(labels)
+
+# # Convert labels to tensor
+# labels = torch.from_numpy(labels)
