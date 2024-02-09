da2d156723e0a16903a332aa01706fa3c31b1ed4
JenniferTsang911 <134722878+JenniferTsang911@users.noreply.github.com>
2024-02-09 13:05:24 -0500

    updated base paths

--- run.py
+++ run.py
@@ -8,8 +8,9 @@ random.seed(42)
 np.random.seed(42)
 tf.random.set_seed(42)
 
-train_data, train_labels, val_data, val_labels, test_data, test_labels, class_names = process_data(
-    r"C:\Users\jenni\Documents\GitHub\DESI-project\all_aligned_no_background_others_preprocessed.csv", balanced=True, should_shuffle=True)
+base_path = ''
+
+train_data, train_labels, val_data, val_labels, test_data, test_labels, class_names = process_data(base_path, balanced=True, should_shuffle=True)
 
 
 learning_rate = 0.001
@@ -17,12 +18,6 @@ input_dim = train_data.shape[1]  # P
 latent_dim = 250  # or any other number you want
 epochs_ae = 1000
 batch_size_AE = 128
-
-# epochs = 500
-# batch_size = 512
-# learning_rateCL = 0.00001
-# l2_lambda = 0.0001
-# dropout_rate = 0.8
 
 epochs = 500
 batch_size = 128
@@ -34,7 +29,7 @@ dropout_rate = 0.2
 # encoded_train_data, encoded_val_data = train_autoencoder(
 #      autoencoder, train_data, val_data, epochs_ae, batch_size_AE, learning_rate)
 
-autoencoder = tf.keras.models.load_model('autoencoder_model_unbalanced_with_tissues')
+autoencoder = tf.keras.models.load_model('autoencoder_model')
 encoded_train_data = autoencoder.encoder.predict(train_data)
 encoded_val_data = autoencoder.encoder.predict(val_data)
 
